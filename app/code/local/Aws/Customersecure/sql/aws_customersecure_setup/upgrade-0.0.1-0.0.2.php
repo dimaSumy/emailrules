f972a45aaf37e620c5ab7b455abf68c5f44b731a
Dima Rudenko <dmitriy.rudenko@aws4.net>
2017-11-21 10:34:13 +0200

    #time 3m Added upgrade script #comment Added upgrade Script

--- app/code/local/Aws/Customersecure/sql/aws_customersecure_setup/upgrade-0.0.1-0.0.2.php
+++ app/code/local/Aws/Customersecure/sql/aws_customersecure_setup/upgrade-0.0.1-0.0.2.php
@@ -1,7 +1,34 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: work
- * Date: 09.11.17
- * Time: 14:25
+ * Absolute Web Services Intellectual Property
+ *
+ * @category     {Aws/FlexFooter}
+ * @copyright    Copyright © 1999-2017 Absolute Web Services, Inc. (http://www.absolutewebservices.com)
+ * @author       Absolute Web Services
+ * @license      http://www.absolutewebservices.com/license-agreement/  Single domain license
+ * @terms of use http://www.absolutewebservices.com/terms-of-use/
  */
+/**
+ * @var $this Mage_Core_Model_Resource_Setup
+ */
+
+$installer = $this;
+
+$tableName = $installer->getTable('aws_customersecure/aws_secure_rules');
+
+$installer->getConnection()->changeColumn($tableName, 'email_groups', 'email_groups', array(
+    'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+    'length'    => null
+));
+
+$installer->getConnection()->changeColumn($tableName, 'cms_pages', 'cms_pages', array(
+    'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+    'length'    => null
+));
+
+$installer->getConnection()->changeColumn($tableName, 'customer_groups', 'customer_groups', array(
+    'type'      => Varien_Db_Ddl_Table::TYPE_TEXT,
+    'length'    => null
+));
+
+$installer->endSetup();
